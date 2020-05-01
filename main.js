997e2734ea351de0cc367a0a77486ef77cd8eec5
eswar2001 <teswar2001@yahoo.co.in>
2020-05-01 09:21:02 +0530

    dynamically building project

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-
+  
     const imgArray = [
         'https://images.unsplash.com/photo-1439792675105-701e6a4ab6f0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1052&q=80',
         'https://images.unsplash.com/photo-1536746803623-cef87080bfc8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1832&q=80',
@@ -23,8 +23,8 @@ $(document).ready(function () {
             'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),url(' + imgArray[
                 interval] + ')');
     }, 4000);
-
 });
+
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
