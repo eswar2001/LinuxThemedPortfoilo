e107db44aec3fbc3e328f21ee6e40752d64a23e9
eswar2001 <teswar2001@yahoo.co.in>
2020-07-10 20:08:24 +0530

    stable

--- main.js
+++ main.js
@@ -269,33 +269,4 @@ $(document).ready(function () {
         );
         console.log(imgArray.length);
     }, 10000);
-    $(window).on('load', function () {
-        var portfolioIsotope = $('.portfolio-container').isotope({
-            itemSelector: '.portfolio-item',
-            layoutMode: 'fitRows'
-        });
-
-        $('#portfolio-flters li').on('click', function () {
-            $("#portfolio-flters li").removeClass('filter-active');
-            $(this).addClass('filter-active');
-
-            portfolioIsotope.isotope({
-                filter: $(this).data('filter')
-            });
-        });
-
-    }); // Initiate venobox (lightbox feature used in portofilo)
-    $(document).ready(function () {
-        $('.venobox').venobox({
-            'share': false
-        });
-    });
-
-    // Portfolio details carousel
-    $(".portfolio-details-carousel").owlCarousel({
-        autoplay: true,
-        dots: true,
-        loop: true,
-        items: 1
-    });
 });
